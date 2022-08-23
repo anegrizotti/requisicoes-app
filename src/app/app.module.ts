ac475cee0b1feb9fce8454b943172ca4ed00cfbe
anegrizotti <algrizotti@gmail.com>
2022-08-23 15:33:36 -0300

    Configurações iniciais do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB4UP2fstbarBCFjdSs0r0w6F15hk_tFEI",
+    authDomain: "requisicoes-app-9a0c7.firebaseapp.com",
+    projectId: "requisicoes-app-9a0c7",
+    storageBucket: "requisicoes-app-9a0c7.appspot.com",
+    messagingSenderId: "139351856650",
+    appId: "1:139351856650:web:daa682b8117c2eae97b1f3"
+  }
 };
 
 /*
