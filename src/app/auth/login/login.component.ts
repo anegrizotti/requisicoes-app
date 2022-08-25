e94607505928d567632370a5e4c07823fdac3751
anegrizotti <algrizotti@gmail.com>
2022-08-25 17:14:12 -0300

    Tela de login e recuperação de senha implementadas

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { AbstractControl, AbstractControlDirective, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
@@ -10,11 +11,13 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class LoginComponent implements OnInit {
   public form: FormGroup;
+  public formRecuperacao: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private modalService: NgbModal
     ) { }
 
   ngOnInit(): void {
@@ -22,6 +25,10 @@ export class LoginComponent implements OnInit {
       email: new FormControl(""),
       senha: new FormControl("")
     });
+
+    this.formRecuperacao = this.formBuilder.group({
+      emailRecuperacao: new FormControl("")
+    })
   }
 
   get email(): AbstractControl | null {
@@ -30,6 +37,10 @@ export class LoginComponent implements OnInit {
 
   get senha(): AbstractControl | null {
     return this.form.get("senha");
+  }
+
+  get emailRecuperacao(): AbstractControl | null {
+    return this.formRecuperacao.get("emailRecuperacao");
   }
 
   public async login() {
@@ -47,6 +58,19 @@ export class LoginComponent implements OnInit {
       console.log(error);
     }
 
+  }
+
+  public abrirModalRecuperacao(modal: TemplateRef<any>) {
+    this.modalService.open(modal)
+      .result
+      .then(resultado => {
+        if(resultado === "enviar") {
+          this.authService.resetarSenha(this.emailRecuperacao?.value);
+        }
+      })
+      .catch(() => {
+        this.formRecuperacao.reset();
+      });
   }
 
 }
