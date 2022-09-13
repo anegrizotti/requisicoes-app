a28d29e55ea8bf5990789d422b1f839131ef78f9
anegrizotti <algrizotti@gmail.com>
2022-09-13 15:43:08 -0300

    Adicionado pasta environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB4UP2fstbarBCFjdSs0r0w6F15hk_tFEI",
+    authDomain: "requisicoes-app-9a0c7.firebaseapp.com",
+    projectId: "requisicoes-app-9a0c7",
+    storageBucket: "requisicoes-app-9a0c7.appspot.com",
+    messagingSenderId: "139351856650",
+    appId: "1:139351856650:web:daa682b8117c2eae97b1f3"
+  }
 };
